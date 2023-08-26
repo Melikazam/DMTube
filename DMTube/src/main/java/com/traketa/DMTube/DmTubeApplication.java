d2a1242c8b00ccf122621e143d01167467738fba
Melikazam <sabamelikishvili@gmail.com>
2023-08-26 16:11:48 +0400

    Project created

---
+++ DMTube/src/main/java/com/traketa/DMTube/DmTubeApplication.java
@@ -0,0 +1,13 @@
+package com.traketa.DMTube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmTubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmTubeApplication.class, args);
+	}
+
+}
