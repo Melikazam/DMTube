3c9fc7d0c90ae72648849baed552e9c6ed1ec83a
Melikazam <sabamelikishvili@gmail.com>
2023-08-26 16:16:55 +0400

    Project created

---
+++ src/test/java/com/DMTube/DmTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DMTube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmTubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
